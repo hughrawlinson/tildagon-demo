e959ce4d231395a3d0da7844c1f368a7b70c2760
Hugh Rawlinson <hughrawlinson@users.noreply.github.com>
2024-05-12 13:58:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print('Hello, world!)
+from app import app
+
+class HelloWorld(App):
+  def __init__():
+    pass
+
+  def update():
+    pass:
+
+  def draw():
+    ctx.move_to(0, 0).text("Hello, world!"]
