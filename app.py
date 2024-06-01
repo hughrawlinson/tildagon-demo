d82c194ffdaeace8f1130626986e79984806e77a
Hugh Rawlinson <hughrawlinson@users.noreply.github.com>
2024-06-01 13:41:18 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
-from app import app
+from app import App
 
 class HelloWorld(App):
-  def __init__():
+  def __init__(self):
     pass
 
-  def update():
+  def update(self):
     pass:
 
-  def draw():
+  def draw(self):
     ctx.move_to(0, 0).text("Hello, world!"]
 
 __app_export__ = HelloWorld
