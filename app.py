5a215bf4062712e60fddfe135b17ed7cae70e997
Hugh Rawlinson <hughrawlinson@users.noreply.github.com>
2024-06-01 13:50:19 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 from app import App
+from app_components import clear_background
 
 class HelloWorld(App):
   def __init__(self):
     pass
 
   def update(self):
-    pass:
+    pass
 
   def draw(self, ctx):
-    ctx.move_to(0, 0).text("Hello, world!"]
+    clear_background(ctx)
+    ctx.text_align = ctx.CENTER
+    ctx.text_baseline = ctx.MIDDLE
+    ctx.move_to(0, 0).gray(1).text("Hello, world!"]
 
 __app_export__ = HelloWorld
